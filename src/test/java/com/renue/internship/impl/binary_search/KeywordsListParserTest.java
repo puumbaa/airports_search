c2e471d75943905174062491e6e697230012b595
Elnur Sardarov <sardarovelnur@gmail.com>
2022-08-11 01:44:05 +0300

    removed test file generation beofore all tests because it turned out it was redundant

--- src/test/java/com/renue/internship/impl/binary_search/KeywordsListParserTest.java
+++ src/test/java/com/renue/internship/impl/binary_search/KeywordsListParserTest.java
@@ -16,27 +16,6 @@ public class KeywordsListParserTest {
     private final Parser<KeywordsList> parser = new KeywordsListParser(FILENAME);
     private static final String FILENAME = "airports-test.csv";
 
-    @BeforeAll
-    public static void create_test_file() {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILENAME))) {
-            writer.write(
-                    "1,\"Goroka Airport\",\"Goroka\",\"Papua New Guinea\",\"GKA\",\"AYGA\",-6.081689834590001,145.391998291,5282,10,\"U\",\"Pacific/Port_Moresby\",\"airport\",\"OurAirports\"\n" +
-                            "2,\"Madang Airport\",\"Madang\",\"Papua New Guinea\",\"MAG\",\"AYMD\",-5.20707988739,145.789001465,20,10,\"U\",\"Pacific/Port_Moresby\",\"airport\",\"OurAirports\"\n" +
-                            "3,\"Mount Hagen Kagamuga Airport\",\"Mount Hagen\",\"Papua New Guinea\",\"HGU\",\"AYMH\",-5.826789855957031,144.29600524902344,5388,10,\"U\",\"Pacific/Port_Moresby\",\"airport\",\"OurAirports\"\n" +
-                            "4,\"Nadzab Airport\",\"Nadzab\",\"Papua New Guinea\",\"LAE\",\"AYNZ\",-6.569803,146.725977,239,10,\"U\",\"Pacific/Port_Moresby\",\"airport\",\"OurAirports\"\n" +
-                            "5,\"Port Moresby Jacksons International Airport\",\"Port Moresby\",\"Papua New Guinea\",\"POM\",\"AYPY\",-9.443380355834961,147.22000122070312,146,10,\"U\",\"Pacific/Port_Moresby\",\"airport\",\"OurAirports\"\n" +
-                            "6,\"Wewak International Airport\",\"Wewak\",\"Papua New Guinea\",\"WWK\",\"AYWK\",-3.58383011818,143.669006348,19,10,\"U\",\"Pacific/Port_Moresby\",\"airport\",\"OurAirports\"\n" +
-                            "7,\"Narsarsuaq Airport\",\"Narssarssuaq\",\"Greenland\",\"UAK\",\"BGBW\",61.1604995728,-45.4259986877,112,-3,\"E\",\"America/Godthab\",\"airport\",\"OurAirports\"\n" +
-                            "8,\"Godthaab / Nuuk Airport\",\"Godthaab\",\"Greenland\",\"GOH\",\"BGGH\",64.19090271,-51.6781005859,283,-3,\"E\",\"America/Godthab\",\"airport\",\"OurAirports\"\n" +
-                            "9,\"Kangerlussuaq Airport\",\"Sondrestrom\",\"Greenland\",\"SFJ\",\"BGSF\",67.0122218992,-50.7116031647,165,-3,\"E\",\"America/Godthab\",\"airport\",\"OurAirports\"\n" +
-                            "10,\"Thule Air Base\",\"Thule\",\"Greenland\",\"THU\",\"BGTL\",76.5311965942,-68.7032012939,251,-4,\"E\",\"America/Thule\",\"airport\",\"OurAirports\"\n" +
-                            "12,\"Egilsstaðir Airport\",\"Egilsstadir\",\"Iceland\",\"EGS\",\"BIEG\",65.2833023071289,-14.401399612426758,76,0,\"N\",\"Atlantic/Reykjavik\",\"airport\",\"OurAirports\"\n" +
-                            "216,\"Aguenar – Hadj Bey Akhamok Airport\",\"Tamanrasset\",\"Algeria\",\"TMR\",\"DAAT\",22.8115005493,5.45107984543,4518,1,\"N\",\"Africa/Algiers\",\"airport\",\"OurAirports\"\n" +
-                            "13,\"Hornafjörður Airport\",\"Hofn\",\"Iceland\",\"HFN\",\"BIHN\",64.295601,-15.2272,24,0,\"N\",\"Atlantic/Reykjavik\",\"airport\",\"OurAirports\"\n");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
 
     @Test
     public void parse_column() {
@@ -66,7 +45,7 @@ public class KeywordsListParserTest {
         Assertions.assertEquals(
                 "13,\"Hornafjörður Airport\",\"Hofn\",\"Iceland\",\"HFN\",\"BIHN\",64.295601,-15.2272,24,0," +
                         "\"N\",\"Atlantic/Reykjavik\",\"airport\",\"OurAirports\"",
-                parser.parseLine(1845));
+                parser.parseLine(1857));
     }
 
 }
