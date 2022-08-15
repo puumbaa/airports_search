a81fb9e17c288c00f0292ff9dd4c4c72a4cc600c
Elnur Sardarov <sardarovelnur@gmail.com>
2022-08-15 20:20:31 +0300

    fixed tests

--- src/test/java/com/renue/internship/impl/binary_search/KeywordsListParserTest.java
+++ src/test/java/com/renue/internship/impl/binary_search/KeywordsListParserTest.java
@@ -4,9 +4,6 @@ import com.renue.internship.common.Parser;
 import com.renue.internship.common.Type;
 import org.junit.jupiter.api.*;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -16,23 +13,24 @@ public class KeywordsListParserTest {
     private final Parser<KeywordsList> parser = new KeywordsListParser(FILENAME);
     private static final String FILENAME = "airports-test.csv";
 
+    private static final int LINE_SEPARATOR_LENTGH = System.lineSeparator().getBytes().length;
 
     @Test
     public void parse_column() {
         KeywordsList expected = new KeywordsList(Arrays.asList(
                 new KeywordsList.KeywordEntry("goroka airport", 0),
-                new KeywordsList.KeywordEntry("madang airport", 153),
-                new KeywordsList.KeywordEntry("mount hagen kagamuga airport", 300),
-                new KeywordsList.KeywordEntry("nadzab airport", 477),
-                new KeywordsList.KeywordEntry("port moresby jacksons international airport", 617),
-                new KeywordsList.KeywordEntry("wewak international airport", 809),
-                new KeywordsList.KeywordEntry("narsarsuaq airport", 968),
-                new KeywordsList.KeywordEntry("godthaab / nuuk airport", 1114),
-                new KeywordsList.KeywordEntry("kangerlussuaq airport", 1259),
-                new KeywordsList.KeywordEntry("thule air base", 1407),
-                new KeywordsList.KeywordEntry("egilsstaðir airport", 1541),
-                new KeywordsList.KeywordEntry("aguenar – hadj bey akhamok airport", 1696),
-                new KeywordsList.KeywordEntry("hornafjörður airport", 1857)
+                new KeywordsList.KeywordEntry("madang airport", 151 + LINE_SEPARATOR_LENTGH),
+                new KeywordsList.KeywordEntry("mount hagen kagamuga airport", 296 + 2L * LINE_SEPARATOR_LENTGH),
+                new KeywordsList.KeywordEntry("nadzab airport", 471 + 3L * LINE_SEPARATOR_LENTGH),
+                new KeywordsList.KeywordEntry("port moresby jacksons international airport", 609 + 4L * LINE_SEPARATOR_LENTGH),
+                new KeywordsList.KeywordEntry("wewak international airport", 799 + 5L * LINE_SEPARATOR_LENTGH),
+                new KeywordsList.KeywordEntry("narsarsuaq airport", 956 + 6L * LINE_SEPARATOR_LENTGH),
+                new KeywordsList.KeywordEntry("godthaab / nuuk airport", 1100 + 7L * LINE_SEPARATOR_LENTGH),
+                new KeywordsList.KeywordEntry("kangerlussuaq airport", 1243 + 8L * LINE_SEPARATOR_LENTGH),
+                new KeywordsList.KeywordEntry("thule air base", 1389 + 9L * LINE_SEPARATOR_LENTGH),
+                new KeywordsList.KeywordEntry("egilsstaðir airport", 1521 + 10L * LINE_SEPARATOR_LENTGH),
+                new KeywordsList.KeywordEntry("aguenar – hadj bey akhamok airport", 1674 + 11L * LINE_SEPARATOR_LENTGH),
+                new KeywordsList.KeywordEntry("hornafjörður airport", 1833 + 12L * LINE_SEPARATOR_LENTGH)
         ));
         expected.setType(Type.STRING);
         KeywordsList actual = new KeywordsList(new ArrayList<>());
@@ -45,7 +43,7 @@ public class KeywordsListParserTest {
         Assertions.assertEquals(
                 "13,\"Hornafjörður Airport\",\"Hofn\",\"Iceland\",\"HFN\",\"BIHN\",64.295601,-15.2272,24,0," +
                         "\"N\",\"Atlantic/Reykjavik\",\"airport\",\"OurAirports\"",
-                parser.parseLine(1857));
+                parser.parseLine(1833 + 12L * LINE_SEPARATOR_LENTGH));
     }
 
 }
